e8d51d450e2c63ed9439e54392497792546767de
RedC0mrade <e2e4e2e4@mail.ru>
2026-07-17 23:29:05 +0200

    ----

--- 2.1.py
+++ 2.1.py
@@ -1,33 +1,18 @@
-from math import floor
-number = 5
-
-# 1 1 1 1 1
-# 1 2 2 2 1
-# 1 2 3 2 1
-# 1 2 2 2 1
-# 1 1 1 1 1
-
-# 1 1 1 1 1 1
-# 1 2 2 2 2 1
-# 1 2 3 3 2 1
-# 1 2 3 3 2 1
-# 1 2 2 2 2 1
-# 1 1 1 1 1 1
-
-matrix = [[1]*number for _ in range(number)]
-max = floor(number / 2)
-
+number = int(input())
+matrix = [[0]*number for _ in range(number)]
+flag_up = 0
+flag_down = number + 1
 for i in range(number):
+    flag_down -= 1
     for j in range(number):
-        if j >= matrix[i][j]:
-            matrix[i][j] = j+1
-
-print(*matrix, sep="\n")
-
-# 0 1 2 3 4
-# 1 2 3 4 5
-# 1
-# 2
-# 3
-# 4
-# 5
+        if i <= j and i < number - j:
+            matrix[i][j] = i + 1
+        elif i > j and i < number - 1 - j:
+            matrix[i][j] = j + 1
+        elif i < j and i > number - 1 - j:
+            matrix[i][j] = number - j
+        elif i >= j and i >= number - j - 1:
+            matrix[i][j] = number - i
+    
+for i in matrix:
+    print(*i, sep=' ')
