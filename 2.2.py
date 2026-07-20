969ed8f88beb545a3502c9469e92987b284e4e0e
RedC0mrade <e2e4e2e4@mail.ru>
2026-07-20 21:50:27 +0200

    ---

--- 2.2.py
+++ 2.2.py
@@ -1,2 +1,12 @@
-def bracket(string: str):
-    
+def bracket(string: str) -> bool:
+    flag = 0
+    dictionary = {'(': 1, ')': -1}
+    for i in string:
+        flag += dictionary[i]
+        if flag < 0:
+            return False
+    if flag != 0:
+        return False
+    return True
+
+print(bracket(string=input()))
