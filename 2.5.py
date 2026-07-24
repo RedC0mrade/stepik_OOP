14d0e056aa20c607b7a79147f62685ffb1c329da
RedC0mrade <e2e4e2e4@mail.ru>
2026-07-24 23:46:36 +0200

    ---

---
+++ 2.5.py
@@ -0,0 +1,13 @@
+import json
+
+
+def jsonify(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return json.dumps(func(*args, **kwargs))
+    return wrapper
+@jsonify
+def make_user(id, live, options):
+    return {'id': id, 'live': live, 'options': options}
+    
+print(make_user(4, False, None))
