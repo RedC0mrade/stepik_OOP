c7a6965c1932ff9aed85b559341c23793a60a1ec
RedC0mrade <e2e4e2e4@mail.ru>
2026-07-27 22:50:27 +0200

    ---

---
+++ 2.6.py
@@ -0,0 +1,7 @@
+def quantify(iterable, predicate) -> int:
+    return len(list(filter(predicate, iterable)))
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(quantify(numbers, lambda x: x % 2 == 0))
