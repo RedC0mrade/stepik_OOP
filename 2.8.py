171d534ccee36238a8a72837245f3e0d386d32a6
RedC0mrade <e2e4e2e4@mail.ru>
2026-07-29 23:33:48 +0200

    ---

--- 2.8.py
+++ 2.8.py
@@ -1,5 +1,16 @@
 from datetime import date
 
-my_date = date(1992, 10)
+year = int(input())
 
-print(my_date)
+mounth = int(input())
+same_date = []
+for day in range(1, 32):
+    try:
+        my_date = date(year, mounth, day)
+
+        if my_date.strftime('%A') == "Thursday":
+            same_date.append(my_date)
+    except ValueError:
+        break
+
+print(same_date[3].strftime('%d.%m.%y'))
