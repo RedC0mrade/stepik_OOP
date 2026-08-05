c7228f9a8c44fe50a8f0ac33dc4c1c10a98d40e2
RedC0mrade <e2e4e2e4@mail.ru>
2026-08-05 22:20:47 +0200

    ---

--- 2.12.py
+++ 2.12.py
@@ -1,2 +1,10 @@
 def intersperse(iterable, delimiter):
-    
+    iterator = list(iterable)
+    if iterator:
+        for i in iterator[:-1]:
+            yield i
+            yield delimiter
+        yield iterator[-1]
+
+
+
