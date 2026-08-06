4e97be8acca9b3b6ed6c7edfcd9da8cbdcfd1ca8
RedC0mrade <e2e4e2e4@mail.ru>
2026-08-06 21:13:47 +0200

    ---

--- 2.8.py
+++ 2.8.py
@@ -4,7 +4,6 @@ from datetime import date
 def last_thursday(year: int, mounth: int) -> str:
     my_date = date(year, mounth, 1)
     first_thursday = (4 - my_date.isoweekday()) % 7
-    print(first_thursday)
     return date(year, mounth, 1 + first_thursday + 21).strftime("%d.%m.%Y")
 
 
