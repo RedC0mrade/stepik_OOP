50325ea6c51591043ba927c441fdbdd929124da9
RedC0mrade <e2e4e2e4@mail.ru>
2026-08-17 22:21:27 +0200

    ---

--- 2.15.py
+++ 2.15.py
@@ -1,2 +1,16 @@
-def recviz():
-    pass
+def recviz(func):
+    print("->")
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(result)
+    return wrapper
+    print("<-")
+
+
+
+
+@recviz
+def add(a, b):
+    return a + b
+
+add(1, b=2)
