27244dccc1eed7f89084396e493bb113d1739132
RedC0mrade <e2e4e2e4@mail.ru>
2026-08-18 22:21:38 +0200

    ---

--- 2.15.py
+++ 2.15.py
@@ -2,9 +2,10 @@ def recviz(func):
     print("->")
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-        print(result)
+        print(f'вызов {func.__name__}() с аргументами: {args}, {kwargs}')
+        print(f'TRACE: возвращаемое значение {func.__name__}(): {repr(result)}')
+
     return wrapper
-    print("<-")
 
 
 
@@ -14,3 +15,6 @@ def add(a, b):
     return a + b
 
 add(1, b=2)
+
+# -> add(1, b=2)
+# <- 3
