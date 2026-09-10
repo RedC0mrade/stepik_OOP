814e73de72677c71cc95005c6a96dd1a7ec11c0c
RedC0mrade <e2e4e2e4@mail.ru>
2026-09-10 16:36:23 +0200

    ---

---
+++ 4.0/4.16.py
@@ -0,0 +1,13 @@
+class Postman:
+
+    def __init__(self):
+        self.delivery_data = list()
+
+    def add_delivery(street: str, house: int, flat: int):
+        pass
+
+    def get_houses_for_street(street: str):
+        pass
+
+    def get_flats_for_house(number: int):
+        pass
