8e62b342293c2207908a89ccdeab7cff80731fa7
RedC0mrade <e2e4e2e4@mail.ru>
2026-09-23 21:55:41 +0200

    ---

--- 4.0/4.18.py
+++ 4.0/4.18.py
@@ -34,80 +34,56 @@ class Knight:
         board = [["."] * 8 for _ in range(8)]
         vertical = 8 - self.vertical
         board[vertical][horizontal_on_board] = "N"
-
-        try:
-            board[vertical+2][horizontal_on_board+1] = '*'
-        except:
-            pass
-        
-        try:
-            board[vertical+2][horizontal_on_board-1] = '*'
-        except:
-            pass
-        
-        try:
-            board[vertical+1][horizontal_on_board-2] = '*'
-        except:
-            pass
-        
-        try:
-            board[vertical+1][horizontal_on_board+2] = '*'
-        except:
-            pass
-        
-        try:
-            board[vertical-1][horizontal_on_board-2] = '*'
-        except:
-            pass
-        
-        try:
-            board[vertical-1][horizontal_on_board+2] = '*' #!
-        except:
-            pass
-        
-        try:
-            board[vertical-2][horizontal_on_board+1] = '*'
-        except:
-            pass
-        
-        try:
-            board[vertical-2][horizontal_on_board-1] = '*'
-        except:
-            pass
+        knight_move = (
+            (2, 1),
+            (2, -1),
+            (1, -2),
+            (1, 2),
+            (-1, -2),
+            (-1, 2),
+            (-2, 1),
+            (-2, -1)
+            )
+        for move in knight_move:
+            if (v := vertical + move[0]) >= 0 and (h := horizontal_on_board + move[1]) >= 0:
+                try:
+                    board[v][h] = "*"
+                except:
+                    continue
                 
 
         [print(*i) for i in board]
 
 
 
-# # TEST_1:
-# knight = Knight('c', 3, 'white')
+# TEST_1:
+knight = Knight('c', 3, 'white')
 
-# print(knight.color, knight.get_char())
-# print(knight.horizontal, knight.vertical)
+print(knight.color, knight.get_char())
+print(knight.horizontal, knight.vertical)
 
-# # TEST_2:
-# knight = Knight('c', 3, 'white')
+# TEST_2:
+knight = Knight('c', 3, 'white')
 
-# print(knight.horizontal, knight.vertical)
-# print(knight.can_move('e', 5))
-# print(knight.can_move('e', 4))
+print(knight.horizontal, knight.vertical)
+print(knight.can_move('e', 5))
+print(knight.can_move('e', 4))
 
-# knight.move_to('e', 4)
-# print(knight.horizontal, knight.vertical)
+knight.move_to('e', 4)
+print(knight.horizontal, knight.vertical)
 
-# # TEST_3:
-# knight = Knight('c', 3, 'white')
+# TEST_3:
+knight = Knight('c', 3, 'white')
 
-# knight.draw_board()
+knight.draw_board()
 
-# # TEST_4:
-# knight = Knight('e', 5, 'black')
+# TEST_4:
+knight = Knight('e', 5, 'black')
 
-# knight.draw_board()
-# knight.move_to('d', 3)
-# print()
-# knight.draw_board()
+knight.draw_board()
+knight.move_to('d', 3)
+print()
+knight.draw_board()
 
 # TEST_5:
 knight = Knight('a', 1, 'white')
@@ -117,14 +93,14 @@ knight.move_to('e', 8)
 print()
 knight.draw_board()
 
-# # TEST_6:
-# knight = Knight('g', 7, 'black')
-# knight.draw_board()
+# TEST_6:
+knight = Knight('g', 7, 'black')
+knight.draw_board()
 
-# # TEST_7:
-# knight = Knight('d', 8, 'white')
-# knight.draw_board()
+# TEST_7:
+knight = Knight('d', 8, 'white')
+knight.draw_board()
 
-# # TEST_8:
-# knight = Knight('h', 1, 'black')
-# knight.draw_board()
+# TEST_8:
+knight = Knight('h', 1, 'black')
+knight.draw_board()
